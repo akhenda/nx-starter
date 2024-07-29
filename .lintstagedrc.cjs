3e7155980400125e0adedb47fcfdf42e2a4746a4
akhenda <akhenda@gmail.com>
2024-07-29 18:32:57 +0300

    build: :package: update ci workflow

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -13,7 +13,7 @@ module.exports = {
 
   '{apps,libs,packages,tools}/**/*.{js,ts,jsx,tsx,json,md,html,css,scss}': [
     (files) => `nx affected --target=lint --fix --files=${files.join(',')}`,
-    (files) => `nx affected --target=test --files=${files.join(',')}`,
+    // (files) => `nx affected --target=test --files=${files.join(',')}`,
     (files) => `nx format:write --files=${files.join(',')}`,
   ],
 };

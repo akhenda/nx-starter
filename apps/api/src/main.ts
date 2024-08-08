034040d3483077848944b0b8900b38c305c5f692
akhenda <akhenda@gmail.com>
2024-08-08 15:53:28 +0300

    build: :package: convert monorepo to eslint flat-config

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,11 +1,13 @@
-import koa from 'koa';
+/* eslint-disable no-process-env */
+/* eslint-disable no-console */
+import Koa from 'koa';
 
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
-const app = new koa();
+const app = new Koa();
 
-app.use(async (ctx) => {
+app.use((ctx) => {
   ctx.body = { message: 'Hello API' };
 });
 

b712a6a811e64fa735bb9bc5502ca60e1dd5d639
akhenda <akhenda@gmail.com>
2024-08-10 18:10:05 +0300

    test(constants): :white_check_mark: fix env vars

--- libs/configs/constants/test-setup.ts
+++ libs/configs/constants/test-setup.ts
@@ -1,12 +1,25 @@
 // https://gist.github.com/dev99problems/5838f1a437b6fedd7b0aedb81e6c72fb
+// https://stackoverflow.com/a/48042799
 
 const OLD_ENV = process.env;
 
-beforeEach(() => {
-  // MOST IMPORTANT THING IS 👇
-  jest.resetModules(); // Most important - it clears the cache
-  process.env = { ...OLD_ENV }; // Make a copy
-});
+process.env = {
+  // Make a copy
+  ...OLD_ENV,
+  API_KEY: 'fake_api_key',
+  EMAIL_CONFIG_JSON: '{}',
+};
+
+// beforeEach(() => {
+//   // MOST IMPORTANT THING IS 👇
+//   jest.resetModules(); // Most important - it clears the cache
+//   process.env = {
+//     // Make a copy
+//     ...OLD_ENV,
+//     API_KEY: 'fake_api_key',
+//     EMAIL_CONFIG_JSON: '{}',
+//   };
+// });
 
 afterAll(() => {
   process.env = OLD_ENV; // Restore old environment
